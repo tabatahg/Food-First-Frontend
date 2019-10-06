3de213580d0e3b56107678059668e3ec8a21d525
Kirk <kkchu791@gmail.com>
2019-10-05 21:14:14 -0700

    WIP show page

---
+++ foodfirst/src/components/list/OrganizationDetails.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const OrganizationDetails = ({ match }) => {
+  return (
+    <div className="organization-details">
+      <div className="organization-photo">
+        I am a photo
+      </div>
+    </div>
+  );
+}
+
+export default OrganizationDetails;
