c75f32c18147b64a7f8c7e51f614cece38b683ce
Kirk <kkchu791@gmail.com>
2019-10-06 01:24:11 -0700

    add sign up button

--- foodfirst/src/App.js
+++ foodfirst/src/App.js
@@ -6,15 +6,17 @@ import LogIn from "./components/login/LogIn";
 import List from "./components/list/List";
 import TestPage from "./components/list/TestPage";
 import LandingPage from "./components/landingpage/LandingPage";
+import Profile from "./components/profile/Profile";
 
 const App = () => {
   return (
     <Router>
       <div className="App"></div>
-      <Route path="/landingpage" component={LandingPage} />
+      <Route exact={true} path="/" component={LandingPage} />
       <Route path="/login" component={LogIn} />
       <Route path="/testpage" component={TestPage} />
       <Route path="/list" component={List} />
+      <Route path="/profile" component={Profile} />
     </Router>
   );
 };
